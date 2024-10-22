13f732c400f5ba61a2e115538d33649616f4ed57
Eric Pham <eric.42.pham@gmail.com>
2024-10-21 23:41:07 -0400

    Added Try and Catch to server and client and added while loops, code works but only recieved message once, maybe move bufferreader out of while in client

--- src/Client.java
+++ src/Client.java
@@ -2,12 +2,14 @@ import java.io.*;
 import java.net.*;
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("139.62.210.155", 2222);
 
-        BufferedReader receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try(Socket socket = new Socket("139.62.210.155", 2222)) {
+            while(true) {
+                BufferedReader receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        String data = receive.readLine();
-        System.out.println(data);
-
+                String data = receive.readLine();
+                System.out.println(data);
+            }
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,16 +3,25 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(2222);
-        Socket socket = serverSocket.accept();
+        try (ServerSocket serverSocket = new ServerSocket(2222)) {
+            System.out.println("Server is listening on port 2222");
+            while(true) {
+                Socket socket = serverSocket.accept();
 
-        BufferedReader receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //BufferedReader receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        String data = receive.readLine();
-        System.out.println(data);
+                //String data = receive.readLine();
+                //System.out.println(data);
 
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        writer.println("This is a message sent to the server");
-
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                writer.println("This is a message sent from the server");
+                Thread.sleep(1000);
+            }
+        } catch (IOException e){
+            System.out.println("Server Exception: " + e.getMessage());
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
