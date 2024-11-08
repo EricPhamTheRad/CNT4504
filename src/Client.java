b12667e74d2f7b24fb5e68ae3415e297ea164120
Eric Pham <eric.42.pham@gmail.com>
2024-11-07 23:54:28 -0500

    Changed it so it just reads the terminal, sends that string to server and prints what the server sends back

--- src/Client.java
+++ src/Client.java
@@ -1,25 +1,33 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 public class Client {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         //use to terminate program
         boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        String input;
         while (!exit) {
             //try to access server at IP and socket
             try (Socket socket = new Socket("139.62.210.155", 2222)) {
                 //Sets up reading the socket if sucsessful
+                System.out.println("Connected");
                 BufferedReader receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
                 //code to terminate program remotely un comment to activate
-                //while (!exit) {
-                //read data sent from server
-                    String data = receive.readLine();
-                    //if (data.contains("1")) {
-                       // exit = true;
-                    //}
-                    System.out.println(data);
-                //}
-
+                while (!exit) {
+                    //read data sent from server
+                    input = scanner.nextLine();
+                    writer.println(input);
+                    if(input.contains("end")){
+                        exit = true;
+                    }
+                    System.out.println(receive.readLine());
+                }
+                writer.println("closed");
+                writer.close();
+                receive.close();
             } catch (UnknownHostException ex) {
 
                 System.out.println("Server not found: " + ex.getMessage());
